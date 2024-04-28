d6d24bae027a54b321eb6abf0b72e1a2f3168bc0
Sebastian Hernandez <sh-desarrollo@hotmail.com>
2024-04-28 15:37:03 -0500

    initial commit

---
+++ src/test/java/com/projects/api/veterinary/VeterinaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.api.veterinary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
